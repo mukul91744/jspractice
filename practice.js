9995609a91a193e9c19ac2c479995cf775766131
mukul91744 <mukulkushwa7@gmail.com>
2024-02-17 16:08:11 +0530

    i have push my js practice folder for remeber the code of js logic

---
+++ practice.js
@@ -0,0 +1,19 @@
+const inp = document.createElement('input');
+const button = document.createElement('button');
+button.innerHTML = 'button'
+const atrr = inp.setAttribute('type','text');
+const place = inp.setAttribute('placeholder','Please Enter The Name');
+document.write(document.body.append(inp,button))
+
+
+button.addEventListener('click',()=> {
+    const inptval = document.querySelector('input').value;
+    let p = document.querySelector('p');
+    p.innerHTML = '';
+    try {
+        if(inptval == "") throw 'Please Fill the input';
+        if(isNaN(inptval)) throw 'NOt a Number';
+    } catch (error) {
+        p.innerHTML = error;            
+    }
+});
