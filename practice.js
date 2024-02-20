eb854aed668b0ce6360e092b9cb1c9ee719c5738
mukul91744 <mukulkushwa7@gmail.com>
2024-02-20 19:26:57 +0530

    i have develop some code in js dynamically

--- practice.js
+++ practice.js
@@ -1,19 +1,12 @@
-const inp = document.createElement('input');
-const button = document.createElement('button');
-button.innerHTML = 'button'
-const atrr = inp.setAttribute('type','text');
-const place = inp.setAttribute('placeholder','Please Enter The Name');
-document.write(document.body.append(inp,button))
-
-
-button.addEventListener('click',()=> {
-    const inptval = document.querySelector('input').value;
-    let p = document.querySelector('p');
-    try {
-        if(inptval == "") throw 'Please Fill the input';
-        if(isNaN(inptval)) throw 'NOt a Number';
-    } catch (error) {
-        p.innerHTML = error;            
-    }
-    p.style.color = 'red    '
-});
+// Json 
+const emp = '{"name" : "anuj","Lastname" : "Yadav"}'
+let obj = JSON.parse(emp);
+console.log(obj.name+ obj.Lastname);
+const sel = document.createElement("select");
+const option3 = document.createElement("option");
+option3.innerHTML = obj.name;
+let option2 = document.createElement("option");
+option2.innerHTML = obj.Lastname;
+sel.appendChild(option3);
+sel.appendChild(option2);
+const bd = document.body.append(sel);
